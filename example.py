7aac4a1884cf3ebb3a7a053429260a234890c826
Robert Pottorff <rpottorff@gmail.com>
2016-02-08 15:56:29 -0700

    added android commands

--- example.py
+++ example.py
@@ -2,22 +2,29 @@ import Holodeck
 import time
 
 if __name__ == "__main__":
-    agent = Holodeck.UAVAgent(hostname="localhost", port=8989, agentName="RobertsUAV")
+    # agent = Holodeck.UAVAgent(hostname="localhost", port=8989, agentName="RobertsUAV_7")
+    #
+    # def onCamera(data):
+    #     print data
+    #
+    # agent.subscribe("RawCameraSensorArray2D", onCamera)
+    #
+    # while True:
+    #     agent.command().setLocalRotation(5,5,5).send()
+    #     time.sleep(1)
+    #
+    #
+    # agent.kill()
 
-    def onCamera(data):
-        print data
 
-    agent.subscribe("RawCameraSensorArray2D", onCamera)
+    agent = Holodeck.AndroidAgent(hostname="localhost", port=8989, agentName="RobertsAndroid")
 
     while True:
-        agent.command().setLocalRotation(5,5,5).send()
+        print agent.command().setBoneConstraint("bone1", 1, 2, 3, 2).send()
         time.sleep(1)
 
 
     agent.kill()
-
-
-
 
 
 
