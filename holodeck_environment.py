725523520a46a4d766c329a8814f6ae8ecd7ebf4
Joshua Greaves <joshua.greaves@googlemail.com>
2016-12-16 13:17:17 -0700

    Handled getting camera data

--- holodeck_environment.py
+++ holodeck_environment.py
@@ -2,6 +2,8 @@ import Holodeck.holodeck_agents as ha
 import numpy as np
 import json
 import base64
+import io
+from scipy.misc import imread
 import cv2
 
 class HolodeckEnvironment:
@@ -45,22 +47,28 @@ class HolodeckEnvironment:
 
         state = self.AGENT.get_state()
 
-        np_images = []
+        sensor_data_list = []
+        sensor_data = np.empty([1, 0])
+
+        if "ViewportClient" in state:
+            viewport_readings = state["ViewportClient"][1:-2].split(",")
+            int_viewport_readings = map(int, viewport_readings)
+            encoded_readings = bytearray(int_viewport_readings)
+
+            fd = io.BytesIO( encoded_readings )
+            np_img = imread( fd )
+            out_np_img = cv2.cvtColor(np_img[:,:,:-1], cv2.COLOR_BGR2RGB)
+            if len(np_img.shape) > 1:
+                sensor_data_list.append(out_np_img)
+
         if "CameraSensorArray2D" in state:
             sensor = json.loads(state["CameraSensorArray2D"])
             for obj in sensor:
                 for camera,base64_image in obj.items():
                     img = base64.b64decode(base64_image)
-                    
-                    npimg = np.fromstring(img, dtype=np.uint8)
-                    np_images.append(npimg)
 
-        other_outputs = []
-        if "Score" in state:
-            other_outputs.append(state["Score"])
-
-        if "Terminal" in state:
-            other_outputs.append(state["Terminal"])
+                    np_img = np.fromstring(img, dtype=np.uint8)
+                    sensor_data_list.append(np_img)
 
         if "RelativeSkeletalPositionSensor" in state:
             skel_arr = []
@@ -70,27 +78,28 @@ class HolodeckEnvironment:
                 skel_arr.append(obj["Quaternion"]["Y"])
                 skel_arr.append(obj["Quaternion"]["Z"])
                 skel_arr.append(obj["Quaternion"]["W"])
-            other_outputs.append(skel_arr)
+            sensor_data = np.concatenate((sensor_data, np.array([skel_arr])), axis=1)
 
         if "JointRotationSensor" in state:
             joint_arr = []
             joint_rotations = json.loads(state["JointRotationSensor"])
             for obj in joint_rotations:
                 joint_arr.append(obj)
-            other_outputs.append(joint_arr)
+            sensor_data = np.concatenate((sensor_data, np.array([joint_arr])), axis=1)
 
         if "IMUSensor" in state:
             imu_arr = []
-            imu_readings = json.loads(state["JointRotationSensor"])
+            imu_readings = json.loads(state["IMUSensor"])
             for obj in imu_readings:
                 imu_arr.append(obj)
-            other_outputs.append(imu_arr)
+            sensor_data = np.concatenate((sensor_data, np.array([imu_arr])), axis=1)
 
         # if "PressureSensor" in state:
         #     pressure_readings = json.loads(state["PressureSensor"])
         #     output["PressureSensor"] = state["PressureSensor"]
 
-        output = np.array([np_images,other_outputs])
-        print(output)
+        sensor_data_list.append(sensor_data)
 
-        return output
+        return (sensor_data_list,
+                state["Score"] if "Score" in state else None,
+                state["Terminal"] if "Terminal" in state else None)
