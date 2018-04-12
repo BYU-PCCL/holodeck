953eecfb4f0fc84515e33be2df093487ae0da566
nickwalton <nickwalton00@gmail.com>
2018-04-12 03:19:32 -0600

    Added ability for user to specify width and height must have updated holodeck build as well to function

--- Holodeck/Environments.py
+++ Holodeck/Environments.py
@@ -70,6 +70,8 @@ class HolodeckEnvironment(object):
         self._width = width
         self._uuid = uuid
 
+        Sensors.set_primary_cam_size(width, height)
+
         if start_world:
             if os.name == "posix":
                 self.__linux_start_process__(binary_path, task_key, gl_version)
