c65cd973636ff089a017af9e1031f7d43f6b1bb3
Jayden Milne <jaydenmilne@users.noreply.github.com>
2018-11-27 10:43:25 -0700

    Add RenderViewport command, option to not show Viewport
    Adds (1) a command to toggle viewport rendering and (2) an option to run headless on Linux. Part of #135
    
    The command to toggle viewport rendering is exposed to the user with the should_render_viewport method on HolodeckEnvironment. Depends on
    BYU-PCCL/holodeck-engine@8684316966bea433ed58e68238ed0a2e71fb841f

--- holodeck/holodeck.py
+++ holodeck/holodeck.py
@@ -19,7 +19,7 @@ class GL_VERSION(object):
     OPENGL3 = 3
 
 
-def make(world_name, gl_version=GL_VERSION.OPENGL4, window_res=None, cam_res=None, verbose=False):
+def make(world_name, gl_version=GL_VERSION.OPENGL4, window_res=None, cam_res=None, verbose=False, show_viewport=True):
     """Creates a holodeck environment using the supplied world name.
 
     Args:
@@ -29,7 +29,7 @@ def make(world_name, gl_version=GL_VERSION.OPENGL4, window_res=None, cam_res=Non
         window_res ((int, int), optional): The resolution to load the game window at. Defaults to (512, 512).
         cam_res ((int, int), optional): The resolution to load the pixel camera sensors at. Defaults to (256, 256).
         verbose (bool): Whether to run in verbose mode. Defaults to False.
-
+        show_viewport (bool): If the viewport window should be shown on-screen (Linux only). Defaults to True
     Returns:
         HolodeckEnvironment: A holodeck environment instantiated with all the settings necessary for the specified
             world, and other supplied arguments.
@@ -43,10 +43,12 @@ def make(world_name, gl_version=GL_VERSION.OPENGL4, window_res=None, cam_res=Non
     param_dict["uuid"] = str(uuid.uuid4())
     param_dict["gl_version"] = gl_version
     param_dict["verbose"] = verbose
+    param_dict["show_viewport"] = show_viewport
 
     if window_res is not None:
         param_dict["window_width"] = window_res[0]
         param_dict["window_height"] = window_res[1]
+
     if cam_res is not None:
         param_dict["camera_width"] = cam_res[0]
         param_dict["camera_height"] = cam_res[1]
