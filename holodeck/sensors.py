804786f333687f49d176959dd149f3f5bd222b8c
nickwalton <nickwalton00@gmail.com>
2019-01-04 19:41:13 -0700

    First commit

--- holodeck/sensors.py
+++ holodeck/sensors.py
@@ -2,6 +2,188 @@
 import numpy as np
 from holodeck.exceptions import HolodeckException
 
+class Sensor(object):
+
+    def __init__(self, client, name="DefaultSensor"):
+        self.name = name
+        self._client = client
+
+        self._on_bool_buffer = self._client.malloc(name + "_teleport_flag", [1], np.uint8)
+        self._sensor_data_buffer = self._client.malloc(name, self.data_shape, self.dtype)
+
+    @property
+    def dtype(self):
+        """The type of data in the sensor
+
+        Returns:
+            numpy dtype of sensor data
+        """
+        raise NotImplementedError("Child class must implement this property")
+
+    @property
+    def data_shape(self):
+        """The shape of the sensor data
+
+        Returns:
+            tuple representing sensor data shape
+        """
+        raise NotImplementedError("Child class must implement this property")
+
+
+class Terminal(Sensor):
+
+    @property
+    def dtype(self):
+        return np.bool
+
+    @property
+    def data_shape(self):
+        return [1]
+
+
+class Reward(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [1]
+
+
+class ViewportCapture(Sensor):
+
+    def __init__(self, client, name="ViewportCapture", shape=(512, 512, 4)):
+        super(ViewportCapture, self).__init__(client, name=name)
+        self.shape = shape
+
+    @property
+    def dtype(self):
+        return np.uint8
+
+    @property
+    def data_shape(self):
+        return self.shape
+
+
+class RGBCamera(Sensor):
+
+    def __init__(self, client, name="ViewportCapture", shape=(256, 256, 4)):
+        super(RGBCamera, self).__init__(client, name=name)
+        self.shape = shape
+
+    @property
+    def dtype(self):
+        return np.uint8
+
+    @property
+    def data_shape(self):
+        return self.shape
+
+
+class OrientationSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [3, 3]
+
+
+class IMUSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [2, 3]
+
+
+class JointRotationSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [94]
+
+
+class RelativeSkeletalPositionSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [67, 4]
+
+
+class LocationSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [3]
+
+
+class RotationSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [3]
+
+
+class VelocitySensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [3]
+
+
+class CollisionSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.bool
+
+    @property
+    def data_shape(self):
+        return [1]
+
+
+class PressureSensor(Sensor):
+
+    @property
+    def dtype(self):
+        return np.float32
+
+    @property
+    def data_shape(self):
+        return [48*(3+1)]
+
+
+
+
+
 class Sensors:
     """Class information of sensor data with mappings from names to corresponding numbers
 
