09954621c3dbd7dc197fe866d361d8c10e159b79
nickwalton <nickwalton00@gmail.com>
2019-04-29 18:49:35 -0600

    fixed tests

--- holodeck/tests/test_utils.py
+++ holodeck/tests/test_utils.py
@@ -1,9 +1,13 @@
+import numpy as np
 
 def almost_equal(item1, item2, thresh=0.01):
+
+    item1 = np.array(item1).flatten()
+    item2 = np.array(item2).flatten()
     if len(item1) != len(item2):
         return False
     else:
         for i, val1 in enumerate(item1):
-            if abs(val1 - item2[i]) > thresh*val1:
+            if abs(val1 - item2[i]) > thresh*val1 and val1 > 0.01:
                 return False
     return True
