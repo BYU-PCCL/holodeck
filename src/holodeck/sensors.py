a673e48b199ccdcabb20d4c39670af7233c8d9d2
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-06-13 12:37:33 -0600

    Add some missing sensor docstrings

--- src/holodeck/sensors.py
+++ src/holodeck/sensors.py
@@ -416,7 +416,9 @@ class RotationSensor(HolodeckSensor):
 
 
 class VelocitySensor(HolodeckSensor):
-
+    """Returns the x, y, and z velocity of the agent.
+    
+    """
     sensor_type = "VelocitySensor"
 
     @property
@@ -429,6 +431,9 @@ class VelocitySensor(HolodeckSensor):
 
 
 class CollisionSensor(HolodeckSensor):
+    """Returns true if the agent is colliding with anything (including the ground).
+    
+    """
 
     sensor_type = "CollisionSensor"
 
