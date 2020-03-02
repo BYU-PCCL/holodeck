f905e2e005fe1aaa37fa3106e752e23de3f38f60
Vin Howe <24789592+iiridescent@users.noreply.github.com>
2020-03-02 14:17:10 -0700

    Add day cycle tests, reformat

--- tests/worlds/mazeworld/test_weather.py
+++ tests/worlds/mazeworld/test_weather.py
@@ -8,7 +8,10 @@ from _pytest.fixtures import FixtureRequest
 from holodeck import HolodeckException
 from holodeck.environments import HolodeckEnvironment
 
-from tests.utils.captures import compare_rgb_with_baseline
+from tests.utils.captures import (
+    compare_rgb_with_baseline,
+    write_image_from_rgb_sensor_data,
+)
 from tests.worlds.mazeworld.conftest import env_with_config
 
 # TODO: Maybe we should consider creating a config used across the board for
@@ -25,15 +28,15 @@ weather_config = {
                 {
                     "sensor_type": "RGBCamera",
                     "socket": "CameraSocket",
-                    "sensor_name": "TestCamera"
+                    "sensor_name": "TestCamera",
                 }
             ],
             "control_scheme": 0,
-            "location": [.95, -1.75, .5]
+            "location": [0.95, -1.75, 0.5],
         }
     ],
     "window_width": 1024,
-    "window_height": 1024
+    "window_height": 1024,
 }
 
 weather_type_test_data = [
@@ -51,15 +54,23 @@ fog_density_test_data = [
 
 time_test_data = [
     # hour, max_err
-    pytest.param(0, 1000, id="Time 0"),
+    pytest.param(1, 1000, id="Time 0"),
     pytest.param(12, 1000, id="Time 12"),
-    pytest.param(23, 1000, id="Time 23")
+    pytest.param(23, 1000, id="Time 23"),
+]
+
+day_cycle_test_data = [
+    # cycle_length, max_err_before, max_err_after
+    pytest.param(1, 500, 1000, 1000, id="1 minute day cycle"),
+    pytest.param(5, 500, 1000, 1000, id="5 minute day cycle"),
+    pytest.param(30, 500, 1000, 1000, id="30 minute day cycle"),
 ]
 
 
 @pytest.mark.parametrize("weather_type, max_err", weather_type_test_data)
-def test_weather_type_scenario(weather_type: str, max_err: float,
-                               request: FixtureRequest) -> None:
+def test_weather_type_scenario(
+    weather_type: str, max_err: float, request: FixtureRequest
+) -> None:
     """Validate that weather type is loaded correctly from scenario by
     comparing RGB sensor data with saved baseline
     data
@@ -76,15 +87,18 @@ def test_weather_type_scenario(weather_type: str, max_err: float,
 
     with env_with_config(config) as env:
         env.tick(5)
-        err = compare_rgb_with_baseline(env.tick()["TestCamera"],
-                                        request.fspath.dirname,
-                                        f"weather_type_{weather_type}")
+        err = compare_rgb_with_baseline(
+            env.tick()["TestCamera"],
+            request.fspath.dirname,
+            f"weather_type_{weather_type}",
+        )
         assert err < max_err
 
 
 @pytest.mark.parametrize("fog_density, max_err", fog_density_test_data)
-def test_weather_fog_density_scenario(fog_density: float, max_err: float,
-                                      request: FixtureRequest) -> None:
+def test_weather_fog_density_scenario(
+    fog_density: float, max_err: float, request: FixtureRequest
+) -> None:
     """Validate that fog density is loaded correctly from scenario by
     comparing RGB sensor data with saved baseline data
     image
@@ -102,15 +116,65 @@ def test_weather_fog_density_scenario(fog_density: float, max_err: float,
     with env_with_config(config) as env:
         env.tick(5)
 
-        err = compare_rgb_with_baseline(env.tick()["TestCamera"],
-                                        request.fspath.dirname,
-                                        f"weather_fog_density_{fog_density}")
+        err = compare_rgb_with_baseline(
+            env.tick()["TestCamera"],
+            request.fspath.dirname,
+            f"weather_fog_density_{fog_density}",
+        )
         assert err < max_err
 
 
+@pytest.mark.parametrize(
+    "cycle_length, ticks, max_err_before, max_err_after", day_cycle_test_data
+)
+def test_weather_day_cycle_scenario(
+    cycle_length: float,
+    ticks: int,
+    max_err_before: float,
+    max_err_after: float,
+    request: FixtureRequest,
+) -> None:
+    """Verify that day cycle can be set with scenario by comparing RGB sensor
+    data with saved baseline data
+
+    Args:
+        cycle_length (:obj:`float`): The hour in 24-hour format: [0, 23].
+        ticks (:obj:`int`): number of ticks between captures
+        max_err_before: maximum mean squared error between sensor data and
+        baseline for `before` image
+        max_err_before: maximum mean squared error between sensor data and
+        baseline for `after` image
+        data allowed for test to pass
+        request:
+
+    """
+    config = weather_config.copy()
+    config["weather"] = {"day_cycle_length": cycle_length}
+
+    with env_with_config(config) as env:
+        env.tick(5)
+        err_before = compare_rgb_with_baseline(
+            env.tick()["TestCamera"],
+            request.fspath.dirname,
+            f"weather_time_before_{cycle_length}",
+        )
+
+        env.tick(ticks)
+
+        err_after = compare_rgb_with_baseline(
+            env.tick()["TestCamera"],
+            request.fspath.dirname,
+            f"weather_time_after_{cycle_length}",
+        )
+
+        assert err_before < max_err_before
+        assert err_after < max_err_after
+
+
 @pytest.mark.parametrize("hour, max_err", time_test_data)
-def test_weather_time_scenario(hour: float, max_err: float,
-                               request: FixtureRequest) -> None:
+def test_weather_time_scenario(
+    hour: float, max_err: float, request: FixtureRequest
+) -> None:
     """Validate that time can be set with scenario by comparing RGB sensor
     data with saved baseline data
 
@@ -129,9 +193,11 @@ def test_weather_time_scenario(hour: float, max_err: float,
         # write_image_from_rgb_sensor_data(env.tick()["TestCamera"],
         #                                  request.fspath.dirname,
         #                                  f"weather_time_{hour}")
-        err = compare_rgb_with_baseline(env.tick()["TestCamera"],
-                                        request.fspath.dirname,
-                                        f"weather_time_{hour}")
+        err = compare_rgb_with_baseline(
+            env.tick()["TestCamera"],
+            request.fspath.dirname,
+            f"weather_time_{hour}",
+        )
         assert err < max_err
 
 
@@ -150,9 +216,12 @@ def test_fail_incorrect_weather_type_scenario():
 
 
 @pytest.mark.parametrize("weather_type, max_err", weather_type_test_data)
-def test_weather_type_programmatic(weather_type: str, max_err: float,
-                                   weather_env: HolodeckEnvironment,
-                                   request: FixtureRequest) -> None:
+def test_weather_type_programmatic(
+    weather_type: str,
+    max_err: float,
+    weather_env: HolodeckEnvironment,
+    request: FixtureRequest,
+) -> None:
     """Validate that weather type can be set programmatically by comparing
     RGB sensor data with saved baseline data
 
@@ -166,41 +235,22 @@ def test_weather_type_programmatic(weather_type: str, max_err: float,
     """
     weather_env.weather.set_weather(weather_type)
     weather_env.tick(5)
-    err = compare_rgb_with_baseline(weather_env.tick()["TestCamera"],
-                                    request.fspath.dirname,
-                                    f"weather_type_{weather_type}")
-    assert err < max_err
-
-
-@pytest.mark.parametrize("fog_density, max_err", fog_density_test_data)
-def test_weather_fog_density_programmatic(fog_density: float, max_err: float,
-                                          weather_env: HolodeckEnvironment,
-                                          request: FixtureRequest) -> None:
-    """Validate that fog density can be set programmatically comparing RGB
-    sensor data with saved baseline data
-
-    Args:
-        fog_density: density of fog in interval [0, 1]
-        max_err: maximum mean squared error between sensor data and baseline
-        weather_env: environment fixture shared by programmatic tests
-        data allowed for test to pass
-        request:
-
-    """
-    weather_env.weather.set_fog_density(fog_density)
-    weather_env.tick(5)
-    err = compare_rgb_with_baseline(weather_env.tick()["TestCamera"],
-                                    request.fspath.dirname,
-                                    f"weather_fog_density_{fog_density}")
-
+    err = compare_rgb_with_baseline(
+        weather_env.tick()["TestCamera"],
+        request.fspath.dirname,
+        f"weather_type_{weather_type}",
+    )
     assert err < max_err
 
 
 @pytest.mark.parametrize("hour, max_err", time_test_data)
-def test_weather_time_programmatic(hour: float, max_err: float,
-                                   weather_env: HolodeckEnvironment,
-                                   request: FixtureRequest) -> None:
-    """Validate that time can be set programmatically comparing RGB
+def test_weather_time_programmatic(
+    hour: float,
+    max_err: float,
+    weather_env: HolodeckEnvironment,
+    request: FixtureRequest,
+) -> None:
+    """Validate that time can be set programmatically by comparing RGB
     sensor data with saved baseline data
 
     Args:
@@ -213,15 +263,93 @@ def test_weather_time_programmatic(hour: float, max_err: float,
     """
     weather_env.weather.set_day_time(hour)
     weather_env.tick(5)
-    err = compare_rgb_with_baseline(weather_env.tick()["TestCamera"],
-                                    request.fspath.dirname,
-                                    f"weather_time_{hour}")
+    err = compare_rgb_with_baseline(
+        weather_env.tick()["TestCamera"],
+        request.fspath.dirname,
+        f"weather_time_{hour}",
+    )
+
+    assert err < max_err
+
+
+@pytest.mark.parametrize(
+    "cycle_length, ticks, max_err_before, max_err_after", day_cycle_test_data
+)
+def test_weather_day_cycle_programmatic(
+    cycle_length: float,
+    ticks: int,
+    max_err_before: float,
+    max_err_after: float,
+    weather_env: HolodeckEnvironment,
+    request: FixtureRequest,
+) -> None:
+    """Verify that day cycle can be started programmatically by comparing RGB
+    sensor data with saved baseline data
+
+    Args:
+        cycle_length (:obj:`float`): The hour in 24-hour format: [0, 23].
+        ticks (:obj:`int`): number of ticks between captures
+        max_err_before: maximum mean squared error between sensor data and
+        baseline for `before` image
+        max_err_before: maximum mean squared error between sensor data and
+        baseline for `after` image
+        weather_env: environment fixture shared by programmatic tests
+        data allowed for test to pass
+        request:
+
+    """
+    weather_env.tick(5)
+    weather_env.weather.start_day_cycle(cycle_length)
+    err_before = compare_rgb_with_baseline(
+        weather_env.tick()["TestCamera"],
+        request.fspath.dirname,
+        f"weather_time_before_{cycle_length}",
+    )
+
+    weather_env.tick(ticks)
+
+    err_after = compare_rgb_with_baseline(
+        weather_env.tick()["TestCamera"],
+        request.fspath.dirname,
+        f"weather_time_after_{cycle_length}",
+    )
+
+    assert err_before < max_err_before
+    assert err_after < max_err_after
+
+
+@pytest.mark.parametrize("fog_density, max_err", fog_density_test_data)
+def test_weather_fog_density_programmatic(
+    fog_density: float,
+    max_err: float,
+    weather_env: HolodeckEnvironment,
+    request: FixtureRequest,
+) -> None:
+    """Validate that fog density can be set programmatically by comparing RGB
+    sensor data with saved baseline data
+
+    Args:
+        fog_density: density of fog in interval [0, 1]
+        max_err: maximum mean squared error between sensor data and baseline
+        weather_env: environment fixture shared by programmatic tests
+        data allowed for test to pass
+        request:
+
+    """
+    weather_env.weather.set_fog_density(fog_density)
+    weather_env.tick(5)
+    err = compare_rgb_with_baseline(
+        weather_env.tick()["TestCamera"],
+        request.fspath.dirname,
+        f"weather_fog_density_{fog_density}",
+    )
 
     assert err < max_err
 
 
 def test_fail_incorrect_weather_type_programmatic(
-        weather_env: HolodeckEnvironment):
+    weather_env: HolodeckEnvironment,
+):
     """
     Validate that an exception is thrown when an invalid weather type is
     specified programmatically
@@ -250,8 +378,10 @@ def weather_env(request: FixtureRequest):
     global cur_programmatic_weather_env
 
     cur_programmatic_test_name = request.function.__name__
-    if cur_programmatic_test_name != last_programmatic_test_name or \
-            cur_programmatic_weather_env is None:
+    if (
+        cur_programmatic_test_name != last_programmatic_test_name
+        or cur_programmatic_weather_env is None
+    ):
         cur_programmatic_weather_env = env_with_config(weather_config)
 
     return cur_programmatic_weather_env
