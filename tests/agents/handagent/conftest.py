cb09c049e2d734c75f2747cf83aed8254e7f5426
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-10-15 15:01:37 -0600

    Use working cache cleanup method for handagent

--- tests/agents/handagent/conftest.py
+++ tests/agents/handagent/conftest.py
@@ -24,25 +24,36 @@ base_handagent_config = {
 
 
 def pytest_generate_tests(metafunc):
-    if 'shared_env' in metafunc.fixturenames:
-        metafunc.parametrize('shared_env', [base_handagent_config], indirect=True)
+    if 'env' in metafunc.fixturenames:
+        metafunc.parametrize('env', [base_handagent_config], indirect=True)
 
 
-@pytest.fixture(scope="package")
-def shared_env(request):
-    cfg = request.param
-    binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
+shared_env = None
 
-    with holodeck.environments.HolodeckEnvironment(scenario=cfg,
-                                                   binary_path=binary_path,
-                                                   show_viewport=False,
-                                                   uuid=str(uuid.uuid4())) as env:
-        # resetting is handled by `env` and cleanup happens immediately after exiting
-        # the module
-        yield env
+
+@pytest.fixture(scope="package", autouse=True)
+def env_cleanup():
+    global shared_env
+
+    yield
+
+    if callable(getattr(shared_env, "__on_exit__", None)):
+        shared_env.__on_exit__()
 
 
 @pytest.fixture
-def env(shared_env):
+def env(request):
+    global shared_env
+
+    if not shared_env:
+        cfg = request.param
+        binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
+        shared_env = holodeck.environments.HolodeckEnvironment(
+            scenario=cfg,
+            binary_path=binary_path,
+            show_viewport=False,
+            uuid=str(uuid.uuid4()),
+        )
+
     shared_env.reset()
     return shared_env
