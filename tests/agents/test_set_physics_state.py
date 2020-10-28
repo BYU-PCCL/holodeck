87b63242a6155c125fbb7ed7ef1f26e306ac86de
unknown <powley.matthew@gmail.com>
2020-10-28 15:18:59 -0600

    fixed how to access the sensors

--- tests/agents/test_set_physics_state.py
+++ tests/agents/test_set_physics_state.py
@@ -7,12 +7,17 @@ from holodeck.environments import HolodeckEnvironment
 def test_set_physics_state():
     env = holodeck.make("MazeWorld-FinishMazeSphere")
 
-    curr_location = holodeck.sensors.LocationSensor(env)
-    curr_rotation = holodeck.sensors.RotationSensor(env)
-    curr_velocity = holodeck.sensors.VelocitySensor(env)
-  
-    env.test_set_physics_state([100,100,50], [50,50,50], [50,0,0], [90,0,0])
+    print(env.tick())
 
-    assert curr_location == holodeck.sensors.LocationSensor(env), "Location did not get set correctly!"
-    assert curr_rotation == holodeck.sensors.RorationSensor(env), "Rotation did not get set correctly!"
-    assert curr_velocity == holodeck.sensors.VelocitySensor(env), "Velocity did not get set correctly!"
+    state = env.tick()
+    curr_location = state["LocationSensor"]
+    curr_rotation = state["RotationSensor"]
+    curr_velocity = state["VelocitySensor"]
+  
+    env.set_physics_state([100,100,50], [50,50,50], [50,0,0], [90,0,0])
+
+    newState = env.tick()
+
+    assert curr_location == newState["LocationSensor"], "Location did not get set correctly!"
+    assert curr_rotation == newState["RotationSensor"], "Rotation did not get set correctly!"
+    assert curr_velocity == newState["VelocitySensor"], "Velocity did not get set correctly!"
