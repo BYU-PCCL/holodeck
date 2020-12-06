ee590d72bc087652f0afde8b7d7ef5367273dcab
unknown <thechrisluangrath@gmail.com>
2020-12-05 17:58:57 -0700

    test naming/ordering

--- tests/agents/test_max_height.py
+++ tests/agents/test_max_height.py
@@ -67,26 +67,6 @@ def test_max_height_set():
         # The threshold is 12 (11.8283691) because for whatever reason, the UAV goes slightly above the set max height. No one knows why.
 
 
-def test_default_height():
-    """Make sure that not setting a max height will default to having no max height set.
-    """
-
-    with holodeck.environments.HolodeckEnvironment(scenario=no_height_config,
-                                                   binary_path=binary_path,
-                                                   show_viewport=False,
-                                                   uuid=str(uuid.uuid4())) as env:
-
-        command = [0, 0, 0, 1000]
-        state = env.tick()
-        old_location = state["LocationSensor"]
-
-        env.act("uav0", command)
-        state = env.tick(50)
-        current_location = state["LocationSensor"]
-
-        assert ((old_location[2] < current_location[2]) and not (almost_equal(old_location[2], current_location[2]))), "UAV stopped moving up despite no set max height!"
-
-
 def test_stuck_at_max_height():
     """Make sure that the agent doesn't get stuck at the max height after it is first stopped.
     """
@@ -109,3 +89,23 @@ def test_stuck_at_max_height():
         current_location = state["LocationSensor"]
 
         assert ((current_location[2] < max_height) and not (almost_equal(current_location[2], max_height))), "UAV did not fall from max height!"
+
+
+def test_no_max_height_set():
+    """Make sure that not setting a max height will default to not applying a max height.
+    """
+
+    with holodeck.environments.HolodeckEnvironment(scenario=no_height_config,
+                                                   binary_path=binary_path,
+                                                   show_viewport=False,
+                                                   uuid=str(uuid.uuid4())) as env:
+
+        command = [0, 0, 0, 1000]
+        state = env.tick()
+        old_location = state["LocationSensor"]
+
+        env.act("uav0", command)
+        state = env.tick(50)
+        current_location = state["LocationSensor"]
+
+        assert ((old_location[2] < current_location[2]) and not (almost_equal(old_location[2], current_location[2]))), "UAV stopped moving up despite no set max height!"
