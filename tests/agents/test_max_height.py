300c4876eda4c5846f95d286cd6509e13cbaa87a
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-12-18 18:37:34 -0700

    Black formatting changes

--- tests/agents/test_max_height.py
+++ tests/agents/test_max_height.py
@@ -20,9 +20,9 @@ height_config = {
             ],
             "control_scheme": 0,
             "location": [0, 0, 2],
-            "max_height": 3
+            "max_height": 3,
         }
-    ]
+    ],
 }
 
 no_height_config = {
@@ -39,9 +39,9 @@ no_height_config = {
                 }
             ],
             "control_scheme": 0,
-            "location": [0, 0, 2]
+            "location": [0, 0, 2],
         }
-    ]
+    ],
 }
 
 binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
@@ -58,21 +58,24 @@ def max_height_env():
     global shared_max_height_env
 
     if shared_max_height_env is None:
-        
-        binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
 
-        shared_max_height_env = holodeck.environments.HolodeckEnvironment(scenario=height_config,
-                                                   binary_path=binary_path,
-                                                   show_viewport=False,
-                                                   uuid=str(uuid.uuid4()))
+        binary_path = holodeck.packagemanager.get_binary_path_for_package(
+            "DefaultWorlds"
+        )
+
+        shared_max_height_env = holodeck.environments.HolodeckEnvironment(
+            scenario=height_config,
+            binary_path=binary_path,
+            show_viewport=False,
+            uuid=str(uuid.uuid4()),
+        )
 
     with shared_max_height_env:
         yield shared_max_height_env
 
 
 def test_max_height_set(max_height_env):
-    """Make sure that the agent doesn't travel higher than the set max height.
-    """
+    """Make sure that the agent doesn't travel higher than the set max height."""
     max_height_env.reset()
 
     max_height = height_config["agents"][0]["max_height"]
@@ -83,13 +86,14 @@ def test_max_height_set(max_height_env):
 
     current_location = state["LocationSensor"]
 
-    assert ((current_location[2] < max_height) or almost_equal(current_location[2], max_height, r_thresh=12)), "UAV ignored max height that was set!"
+    assert (current_location[2] < max_height) or almost_equal(
+        current_location[2], max_height, r_thresh=12
+    ), "UAV ignored max height that was set!"
     # The threshold is 12 (11.8283691) because for whatever reason, the UAV goes slightly above the set max height. No one knows why.
 
 
 def test_stuck_at_max_height(max_height_env):
-    """Make sure that the agent doesn't get stuck at the max height after it is first stopped.
-    """
+    """Make sure that the agent doesn't get stuck at the max height after it is first stopped."""
     max_height_env.reset()
 
     max_height = height_config["agents"][0]["max_height"]
@@ -104,17 +108,20 @@ def test_stuck_at_max_height(max_height_env):
 
     current_location = state["LocationSensor"]
 
-    assert ((current_location[2] < max_height) and not (almost_equal(current_location[2], max_height))), "UAV did not fall from max height!"
+    assert (current_location[2] < max_height) and not (
+        almost_equal(current_location[2], max_height)
+    ), "UAV did not fall from max height!"
 
 
 def test_no_max_height_set():
-    """Make sure that not setting a max height will default to not applying a max height.
-    """
+    """Make sure that not setting a max height will default to not applying a max height."""
 
-    with holodeck.environments.HolodeckEnvironment(scenario=no_height_config,
-                                                   binary_path=binary_path,
-                                                   show_viewport=False,
-                                                   uuid=str(uuid.uuid4())) as env:
+    with holodeck.environments.HolodeckEnvironment(
+        scenario=no_height_config,
+        binary_path=binary_path,
+        show_viewport=False,
+        uuid=str(uuid.uuid4()),
+    ) as env:
 
         command = [0, 0, 0, 1000]
         state = env.tick()
@@ -124,4 +131,6 @@ def test_no_max_height_set():
         state = env.tick(50)
         current_location = state["LocationSensor"]
 
-        assert ((old_location[2] < current_location[2]) and not (almost_equal(old_location[2], current_location[2]))), "UAV stopped moving up despite no set max height!"
+        assert (old_location[2] < current_location[2]) and not (
+            almost_equal(old_location[2], current_location[2])
+        ), "UAV stopped moving up despite no set max height!"
