997355541ef348d4da6dfada4a1ce1ef77edc354
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-01-22 13:49:24 -0700

    Update arguments on Linux to use new offscreen rendering method

--- src/holodeck/environments.py
+++ src/holodeck/environments.py
@@ -606,16 +606,21 @@ class HolodeckEnvironment:
         loading_semaphore = \
             posix_ipc.Semaphore('/HOLODECK_LOADING_SEM' + self._uuid, os.O_CREAT | os.O_EXCL,
                                 initial_value=0)
-        # Copy the environment variables and remove the DISPLAY variable to hide viewport
-        # https://answers.unrealengine.com/questions/815764/in-the-release-notes-it-says-the-engine-can-now-cr.html?sort=oldest
-        environment = dict(os.environ.copy())
-        if not show_viewport and 'DISPLAY' in environment:
-            del environment['DISPLAY']
+        arguments = [binary_path, task_key, '-HolodeckOn',
+                     '-LOG=HolodeckLog.txt', '-ForceRes', '-ResX=' + str(self._window_size[1]),
+                     '-ResY=' + str(self._window_size[0]), '--HolodeckUUID=' + self._uuid,
+                     '-TicksPerSec=' + str(self._ticks_per_sec)]
+        if not show_viewport:
+            arguments.append('-RenderOffScreen')
+
+            # Copy the environment variables and remove the DISPLAY variable to hide viewport
+            # https://answers.unrealengine.com/questions/815764/in-the-release-notes-it-says-the-engine-can-now-cr.html?sort=oldest
+            environment = dict(os.environ.copy())
+            if 'DISPLAY' in environment:
+                del environment['DISPLAY']
+
         self._world_process = \
-            subprocess.Popen([binary_path, task_key, '-HolodeckOn', '-opengl' + str(gl_version),
-                              '-LOG=HolodeckLog.txt', '-ForceRes', '-ResX=' + str(self._window_size[1]),
-                              '-ResY=' + str(self._window_size[0]), '--HolodeckUUID=' + self._uuid,
-                              '-TicksPerSec=' + str(self._ticks_per_sec)],
+            subprocess.Popen(arguments,
                              stdout=out_stream,
                              stderr=out_stream,
                              env=environment)
