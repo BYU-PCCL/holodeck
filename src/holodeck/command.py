a2edf4b9a9ea10bb8c9839fdcd96ac42bb14c8cd
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-01-27 13:43:40 -0700

    Black style fix

--- src/holodeck/command.py
+++ src/holodeck/command.py
@@ -211,7 +211,15 @@ class SpawnAgentCommand(Command):
 
     """
 
-    def __init__(self, location, rotation, name, agent_type, max_height=sys.maxsize, is_main_agent=False):
+    def __init__(
+        self,
+        location,
+        rotation,
+        name,
+        agent_type,
+        max_height=sys.maxsize,
+        is_main_agent=False,
+    ):
         super(SpawnAgentCommand, self).__init__()
         self._command_type = "SpawnAgent"
         self.set_location(location)
