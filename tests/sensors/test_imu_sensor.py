a4800deb495fbbe11abceb83f1e5e6c5359d631d
mattpow35 <powley.matthew@gmail.com>
2021-02-08 12:29:38 -0700

    finished tests for IMU Sensor

--- tests/sensors/test_imu_sensor.py
+++ tests/sensors/test_imu_sensor.py
@@ -1,10 +1,8 @@
 """Tests for the IMU sensor attached to the agent"""
 
-import uuid
 import holodeck
 import numpy as np
 import pytest
-import pprint
 from tests.utils.equality import almost_equal
 
 turtle_config = {
@@ -31,7 +29,7 @@ SHARED_IMU_SENSOR_ENV = None
 @pytest.fixture(scope="module")
 def imu_sensor_env():
     """Sets up an environment with different instances of the same test"""
-    
+
     global SHARED_IMU_SENSOR_ENV
 
     if SHARED_IMU_SENSOR_ENV is None:
@@ -47,7 +45,7 @@ def imu_sensor_env():
             start_world=False,
             #uuid=str(uuid.uuid4()),
         )
-    
+
     with SHARED_IMU_SENSOR_ENV:
         yield SHARED_IMU_SENSOR_ENV
 
@@ -56,27 +54,60 @@ def test_imu_sensor_at_rest(imu_sensor_env):
     """Validates that the IMU sensor is correctly reading input when agent is at rest."""
 
     imu_sensor_env.reset()
-
     imu_sensor_env.tick()
     at_rest_sensor_reading = np.array([[0, 0, 9.8], [0, 0, 0]])
 
-    new_state = imu_sensor_env.tick()
+    new_state = imu_sensor_env.tick(20)
     sensed_imu_data = new_state["IMUSensor"]
+    assert almost_equal(
+        at_rest_sensor_reading, sensed_imu_data, 0.5, 0.5
+    ), "The sensor did not read the agent at rest correctly!"
 
-    assert almost_equal(at_rest_sensor_reading, sensed_imu_data), "The sensor did not read the agent at rest correctly!py"
 
+def test_imu_sensor_yaw(imu_sensor_env):
+    """Validates that when a force is applied to rotate the UAV on the z-axis that
+    the change in angular velocity on the z-axis was caught by the sensor"""
 
-def test_imu_sensor_after_applied_force(imu_sensor_env):
-    """Validates that the sensor reads new data when a forces is applied."""
     imu_sensor_env.reset()
-
-    imu_sensor_env.tick()
-    orig_sensor_reading = imu_sensor_env.tick()["IMUSensor"]
+    imu_sensor_env.tick(20)
+    orig_sensor_val = imu_sensor_env.tick()["IMUSensor"]
+    assert almost_equal(orig_sensor_val[1][2], 0.0, 0.1, 0.1)
 
     imu_sensor_env.step([0, 0, 15, 100])
-
     imu_sensor_env.tick(25)
-
     new_sensor_reading = imu_sensor_env.tick()["IMUSensor"]
-    
-    assert not almost_equal(orig_sensor_reading, new_sensor_reading)
+    assert new_sensor_reading[1][2] > orig_sensor_val[1][2]
+
+
+def test_imu_sensor_roll(imu_sensor_env):
+    """Validates that when a rotational force is applied on the x-axis of the UAV that the
+    change in angular velocity on the x-axis and the change in linear acceleration on the
+    y-axis were caught by the sensor."""
+
+    imu_sensor_env.reset()
+    imu_sensor_env.tick(20)
+    orig_sensor_val = imu_sensor_env.tick()["IMUSensor"]
+    assert almost_equal(orig_sensor_val[1][0], 0.0, 0.1, 0.1)
+
+    imu_sensor_env.step([0, 15, 0, 100])
+    imu_sensor_env.tick(25)
+    new_sensor_reading = imu_sensor_env.tick()["IMUSensor"]
+    assert abs(new_sensor_reading[1][0]) > abs(orig_sensor_val[1][0])
+    assert abs(new_sensor_reading[0][1]) > abs(orig_sensor_val[0][1])
+
+
+def test_imu_sensor_pitch(imu_sensor_env):
+    """Validates that when a rotational force is applied on the y-axis of the UAV that the
+    change in angular velocity on the y-axis and the change in linear acceleration on the
+    x-axis were caught by the sensor."""
+
+    imu_sensor_env.reset()
+    imu_sensor_env.tick(20)
+    orig_sensor_val = imu_sensor_env.tick()["IMUSensor"]
+    assert almost_equal(orig_sensor_val[1][1], 0.0, 0.1, 0.1)
+
+    imu_sensor_env.step([15, 0, 0, 100])
+    imu_sensor_env.tick(25)
+    new_sensor_reading = imu_sensor_env.tick()["IMUSensor"]
+    assert abs(new_sensor_reading[1][1]) > abs(orig_sensor_val[1][1])
+    assert abs(new_sensor_reading[0][0]) > abs(orig_sensor_val[0][0])
