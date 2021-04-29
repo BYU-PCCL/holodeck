2398095622354e4c3af8ecb8b1b2eb478e932500
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-04-28 21:41:57 -0600

    Fix environment setup for IMU tests

--- tests/sensors/test_imu_sensor.py
+++ tests/sensors/test_imu_sensor.py
@@ -3,6 +3,7 @@
 import holodeck
 import numpy as np
 import pytest
+import uuid
 from tests.utils.equality import almost_equal
 
 turtle_config = {
@@ -41,10 +42,9 @@ def imu_sensor_env():
 
         SHARED_IMU_SENSOR_ENV = holodeck.environments.HolodeckEnvironment(
             scenario=turtle_config,
-            # binary_path=binary_path,
+            binary_path=binary_path,
             show_viewport=False,
-            start_world=False,
-            # uuid=str(uuid.uuid4()),
+            uuid=str(uuid.uuid4())
         )
 
     with SHARED_IMU_SENSOR_ENV:
